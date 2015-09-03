5804f79a4819d1111291a80628deca7dbac567ff
Aria Stewart <aredridel@dinhe.net>
2015-09-03 09:44:44 -0400

    Insulate against modules whose main is meant to be a CLI tool and exits with usage during startup

--- bin/require-time.js
+++ bin/require-time.js
@@ -7,11 +7,14 @@ var pkg,
     all = 0,
     rt = require('../require-time');
 
+var exit = process.exit;
+process.exit = function noop () { };
 try {
     pkg = require(process.cwd() + '/package.json');
 } catch(e) {
     console.error(e.message);
-    process.exit(1);
+    exit(1);
 }
 
 rt.run(pkg, process.argv[2]);
+exit(0);
